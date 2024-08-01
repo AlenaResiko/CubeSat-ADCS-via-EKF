8712784615e90757f2a0dcb09f2030bb99be9415
Alena Chan <86618362+AlenaResiko@users.noreply.github.com>
2024-08-01 15:34:05 -0400

    Add comments

--- h_t.m
+++ h_t.m
@@ -1,12 +1,15 @@
 function out_vector = h_t(in_vector, B_o)
+% h_t is function mapping state to predicted magnetic field measurement in
+% NED frame.
+% Returns [6x1] vector.
+%
+% Input:
+% in_vector - state vector [7x1].
+% B_o - observed measurement from Magnetic Field block in NED frame.
+
     out_vector = zeros(6,1); % set up dimension
-    out_vector(1:3) = transformationMatrix(in_vector(1:4)) * B_o; % read from pre-calculated predicted_orbit_file predicted measurement.
-    out_vector(4:6) = in_vector(5:7);
+    % convert predicted magnetic field measurement this from NED to Body 
+    % frame using transposed transformation matrix.
+    out_vector(1:3) = transformationMatrix(in_vector(1:4)) * B_o;
+    out_vector(4:6) = in_vector(5:7); % copy predicted angular velocity
 end
-
-
-% check that out_vector(1:3) is normalize SANE)
-
-% normalize at each step and look at angles NOT.SANE
-
-% reduce noise of measurements -> reduced theta SANE
