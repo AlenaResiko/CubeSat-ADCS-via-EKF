67b5949c7b813d3ce181a94489529cd9423feb7e
Ruimian-Zheng <160196024+Ruimian-Zheng@users.noreply.github.com>
2024-08-01 15:53:15 -0400

    Add files via upload

--- Script_Multiplicative_FullSystem.m
+++ Script_Multiplicative_FullSystem.m
@@ -1,32 +1,32 @@
-%NEXT STEP: remember to input the inertial position everytime
-clear all;
+clear all; %clear all variables from previous runs
 
-% save simulation outputs
-num_iterations = 15;
-x_list = zeros(7,num_iterations);
-s_list = zeros(7,7,num_iterations);
-q_rotate_list = zeros(4,num_iterations);
-torq_list = zeros(3,num_iterations);
-theta_err_list = zeros(1,num_iterations);
-w_err_list = zeros(1,num_iterations);
-w_body2inertial_list = zeros(3,num_iterations);
-counter = 1;
+%save simulation outputs to produce results
+num_iterations = 1; % number of iteration of the simulation to run
+x_list = zeros(7,num_iterations); % time series of the estimated state vector 
+s_list = zeros(7,7,num_iterations); % time series of the estimated covariance
+q_rotate_list = zeros(4,num_iterations); % time series of the quaternion offset from target
+torq_list = zeros(3,num_iterations); % time series of torque implemented
+theta_err_list = zeros(1,num_iterations); %time series of the angle estimation error (estimation theta - true theta)
+w_err_list = zeros(1,num_iterations); % time series of the norm of the angular rate estimation error (norm of (estimation w - true w))
+w_body2inertial_list = zeros(3,num_iterations); % time series of the angular rate estimation of body frame with respect to inertial frame
+counter = 1; %index for logging the timeseries
 
-mdl = "Multiplicative_Kalman_FullSystem";
-simIN = Simulink.SimulationInput(mdl);
+%getting the model workspace for modifying variables and simulation
+mdl = "Multiplicative_Kalman_FullSystem"; 
+simIN = Simulink.SimulationInput(mdl); 
 pathSpace = mdl+'/Orbit sim'+'/Spacecraft Dynamics';
 pathSpace_mea = mdl+ '/generateMeasurement' + '/Orbit sim measurement' + '/Spacecraft Dynamics';
 mdlWks = get_param('Multiplicative_Kalman_FullSystem','ModelWorkspace');
 
 
-%set the start/stop time
-stepSize = 0.05;
+%set the time to run one iteration
+stepSize = 1; 
 set_param(mdl,'StopTime',mat2str(stepSize))
 set_param(mdl,'SolverType','Fixed-step');
 set_param(mdl,'FixedStep', mat2str(stepSize))
 assignin(mdlWks,'t_step',(stepSize));
 
-%initialize the very initial parameters for theoratical
+%initialize the very initial parameters for theoratical model
 jd =2458850;
 inertial_x=[3649700.0, 3308200.0, -4676600.0];
 inertial_v= [-2750.8, 6666.4, 2573.4];
@@ -34,7 +34,7 @@ w = [0.02;0.02;-0.01];
 q = [0.2; -0.6 ;-0.5; -0.59160797831];
 torq = [0;0;0];
 
-%initialize the very initial parameters for measurement
+%initialize the very initial parameters for measurement simulation
 jd_mea =2458850 ;
 inertial_x_mea=[3649700.0, 3308200.0, -4676600.0];
 inertial_v_mea= [-2750.8, 6666.4, 2573.4];
@@ -49,34 +49,44 @@ s =[[1.94870414, 1.25588909 ,1.4416761, 1.0780205, 1.49168854, 1.53894753, 1.379
     [1.0780205, 1.62980068, 1.12140846, 1.77837393 ,1.21690337, 0.94427956 ,1.46043147];
     [1.49168854,1.46050557, 1.38322267, 1.21690337, 2.06327445 ,1.39185931, 1.33424815];
     [1.53894753, 1.31460547, 1.58836736 ,0.94427956, 1.39185931, 2.02772691, 1.7277207];
-    [1.37981514, 1.13813509 ,1.34392315, 1.46043147, 1.33424815, 1.7277207, 2.16380015]];
+    [1.37981514, 1.13813509 ,1.34392315, 1.46043147, 1.33424815, 1.7277207, 2.16380015]]; %initial covariance
 
-q_ff = [0.76813986749607;-0.00060774689776115;0.00073833382166474;-0.64028136742373];
+q_ff = [0.76813986749607;-0.00060774689776115;0.00073833382166474;-0.64028136742373]; %initial attitude with respect to fix frame
 
-     % %know the parameters
-     %    configSet = getActiveConfigSet(mdl);
-     %    configSetNames = get_param(configSet,"ObjectParameters"); 
+%noise covariances
+R = 0.5*eye(6); 
+Q = 0.05*eye(7);
+assignin(mdlWks,'NoiseCov',R)
+assignin(mdlWks,'Q_identity',Q)
 
+m=100; % mass of CubeSat
+assignin(mdlWks,'m',m);
+
+%call the simulation for the assigned number of iterations
 for i=(1:num_iterations) 
+    %reassign the variables according to the end result of last iteration
+
+        %inertialVelocity
     assignin(mdlWks,'v_icrf_input',inertial_v);
+        %inertialPosition
     assignin(mdlWks,'x_icrf_input',inertial_x);
+        %estimation covariance
     assignin(mdlWks,'s_input', s);
+        %estimation state
     assignin(mdlWks,'x_input',[q;w]);
+        %the torque from last iteration
     assignin(mdlWks,'prev_torque_input', torq);
-    assignin(mdlWks,'controlRun', false);
-    assignin(mdlWks,'savedTorque', torq);
+        %julian date
     assignin(mdlWks,'jd_input',jd)
     assignin(mdlWks,'q_ff_in',q_ff)
 
-    %reinitialize parameters for the theoratical
+    %reinitialize parameters for the theoratical model
         %julianDate
     set_param(pathSpace, "startDate",mat2str(jd));
         %inertialPosition
     set_param(pathSpace, "inertialPosition",mat2str(inertial_x));
-    %get_param(mdl+'/Spacecraft Dynamics', "inertialPosition");
         %inertialVelocity
     set_param(pathSpace, "inertialVelocity",mat2str(inertial_v));
-    %get_param(mdl+'/Spacecraft Dynamics', "inertialVelocity");
         %attitude
     attitude = transpose(q);
     set_param(pathSpace, "attitude", mat2str(attitude));
@@ -89,10 +99,8 @@ for i=(1:num_iterations)
     set_param(pathSpace_mea, "startDate",mat2str(jd_mea));
         %inertialPosition
     set_param(pathSpace_mea, "inertialPosition",mat2str(inertial_x_mea));
-    %get_param(mdl+'/Spacecraft Dynamics', "inertialPosition");
         %inertialVelocity
     set_param(pathSpace_mea, "inertialVelocity",mat2str(inertial_v_mea));
-    %get_param(mdl+'/Spacecraft Dynamics', "inertialVelocity");
         %attitude
     attitude_mea = transpose(q_mea);
     set_param(pathSpace_mea, "attitude", mat2str(attitude_mea));
@@ -109,7 +117,7 @@ for i=(1:num_iterations)
     DSRef= getDatasetRef(GDERun);
 
     
-    %port "x_icrf_out" 
+    %port "x_icrf_out"
     inertial_x = getOutport(DSRef,"x_icrf_out");
 
     %port "v_ircf_out"
@@ -129,27 +137,30 @@ for i=(1:num_iterations)
     %port "torque_out"
     torq = getOutport(DSRef,"torque_out");
 
-    q_rotate = getOutport(DSRef,"q_monkey_out").';
+    %port "q_rotate_out"
+    q_rotate = getOutport(DSRef,"q_rotate_out").';
     
+    %port "theta_err_out"
     theta_err = getOutport(DSRef,"theta_err_out");
+    
+    %port "w_err_out"
     omega_err = getOutport(DSRef,"w_err_out").';
+
+    %port "w_right_frame" (angular rate of body frame with respect to inertial frame
     w_b2in = getOutport(DSRef,"w_right_frame");
 
     % log outputs
-    x_list(:,counter)=x;
+    x_list(:,counter)=x; 
     q_rotate_list(:,counter)=q_rotate;
     torq_list(:,counter)=torq;
     s_list(:,:, counter) = s;
     w_body2inertial_list(:,counter) = w_b2in;
-
     theta_err_list(:,counter)=theta_err;
     w_err_list(:,counter) = norm(omega_err);
     counter = counter+1;
 
     %port "jy_out" julian date
-    jd_temp=jd;
     jd = getOutport(DSRef,"jy_out");
-    % jd-jd_temp;
 
 
     %measurement system
@@ -165,60 +176,11 @@ for i=(1:num_iterations)
 
 
     %port "jy_mea_out"
-    jd_mea_temp = jd_mea;
     jd_mea = getOutport(DSRef,"jy_mea_out");
-    % jd_mea-jd_mea_temp;
 
 end
 
-% disp(x_list);
-% disp(torq_list);
-% disp(q_rotate_list);
-% disp(s_list);
-
-% visualization quaternions
-time_axis = 1:1:num_iterations;
-theta_state_data = 2*acos(x_list(1,:));
-theta_data = 2*acos(q_rotate_list(1,:));
-trace_data = squeeze(s_list(1,1,:)+s_list(2,2,:)+s_list(3,3,:)+s_list(4,4,:)+s_list(5,5,:)+s_list(6,6,:)+s_list(7,7,:));
-
-% torq1_data = torq_list(1,:).^2;
-% torq2_data = torq_list(2,:).^2;
-% torq3_data = torq_list(3,:).^2;
-% torq_norms_data = (torq1_data(:)+torq2_data(:)+torq3_data(:)).^(1/2);
-% % figure(1)
-% % plot(time_axis,torq_norms_data,"--b");
-% % title('torq norms scaled');
-% figure(1)
-% plot(time_axis,w_body2inertial_list);
-% title('w body to inertial');
-% figure(5)
-% plot(time_axis,torq_list)
-% title('torq')
-% figure(2)
-% plot(time_axis,theta_data,"-om");
-% title('angle offset');
-% figure(3)
-% plot(time_axis,q_rotate_list(2:4,:));
-% title('angle offset 3d');
-% % figure(3)
-% % plot(time_axis,trace_data,"-ob");
-% % title('trace');
-% w1_data = x_list(5,:).^2;
-% w2_data = x_list(6,:).^2;
-% w3_data = x_list(7,:).^2;
-% w_data = x_list(5:7,:);
-% omega_norms_data = (w1_data(:)+w2_data(:)+w3_data(:)).^(1/2);
-% figure(4)
-% plot(time_axis,omega_norms_data,"-or");
-% title('magnitude of angular velocity');
-% 
-% figure(6)
-% plot(time_axis,w_data);
-% title('w')
-
-% figure(8)
-% plot(time_axis,x_list(2,:),time_axis,x_list(3,:),time_axis,x_list(4,:));
+%outputting graphs (note: not every logged values are graphed, but it easy to graph more
 time_axis = linspace(stepSize,stepSize*num_iterations,num_iterations);
 figure(5)
 plot(time_axis,theta_err_list,'DisplayName','Kalman');
@@ -226,6 +188,7 @@ figure(6)
 plot(time_axis,w_err_list,'DisplayName','Kalman');
 legend
 
+% the function to get the output values from the model signals in each iteration
 function v = getOutport(DSRef,portname)
     [el,difName,difIndex] = getElement(DSRef,portname);
     dimension = ndims(el.Values.Data);
@@ -236,5 +199,4 @@ function v = getOutport(DSRef,portname)
     elseif (dimension==3)
         v = el.Values.Data(:,:,end);
     end
-    %[row,col] = size(el.Values.Data)
 end
