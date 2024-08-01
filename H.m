155373c9656febcfbe0c8e4f909069ef2fd01cd6
Alena Chan <86618362+AlenaResiko@users.noreply.github.com>
2024-08-01 14:42:22 -0400

    Add documentation.

--- H.m
+++ H.m
@@ -1,6 +1,12 @@
 function [H_out] = H(in_vector,B_o)
+% H is linearization of h_t function (which maps state to predicted
+% measurement).
+% Returns a [6x7] matrix.
+%
+% Input:
+% in_vector - state vector [7x1].
+% B_o - observed measurement from Magnetic Field block in NED frame.
 
-% finds gradient of y w.r.t. x
 q = in_vector(1:4);
 w = in_vector(5:7);
 % left hand is partial_h/partial_q
@@ -19,7 +25,7 @@ dA_T_dq3 = 2 * [-q(3),  q(2), -q(1);
 dA_T_dq4 = 2 * [-q(4),  q(1),  q(2); 
                 -q(1), -q(4),  q(3); 
                  q(2),  q(3),  q(4)];
-% H_out is 6x7
+
 H_out = [dA_T_dq1 * B_o, dA_T_dq2 * B_o, dA_T_dq3 *B_o, dA_T_dq4 *B_o, zeros(3,3);
          zeros(3,4), eye(3)];
 end
