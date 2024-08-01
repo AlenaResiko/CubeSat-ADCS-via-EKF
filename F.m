e7e23318bcd0b38c29fb81b94923643229e535c8
Alena Chan <86618362+AlenaResiko@users.noreply.github.com>
2024-08-01 14:30:56 -0400

    F with comments

--- F.m
+++ F.m
@@ -1,5 +1,11 @@
 function [F_out] = F(in_vector,J1,J2,J3,tStep)
-% finds gradient of f w.r.t. x
+% F is linearization of state transition function
+%
+% Input:
+% in_vector - state vector [7x1].
+% J1, J2, J3 - moments of inertia along principal axes.
+% tStep - duration of one ADCS iteration.
+
 q = in_vector(1:4);
 w = in_vector(5:7);
 % Omega is 4x4, Lambda is 4x3, 0 is 3x3, Pi is 3x3
