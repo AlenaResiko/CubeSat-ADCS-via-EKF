2122aa1f57a2e23586b4c59bfc043bb1a189ab75
Alena Chan <86618362+AlenaResiko@users.noreply.github.com>
2024-08-01 16:11:24 -0400

    Add comments

--- Omega.m
+++ Omega.m
@@ -1,6 +1,13 @@
 function [Om] = Omega(w)
-% Omega
-Om = [0, -w(1), -w(2), -w(3); 
+% Omega is Jacobian of quaternion derivative (q-dot) w.r.t. quaternion (q).
+%
+% Input:
+% w - angular velocity vector [3x1].
+%
+% Return:
+% [4x4] matrix.
+%
+Om = 0.5 * [0, -w(1), -w(2), -w(3); 
      w(1), 0, w(3), -w(2);
      w(2), -w(3), 0, w(1);
      w(3), w(2), -w(1), 0];
