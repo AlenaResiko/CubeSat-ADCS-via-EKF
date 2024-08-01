a76a98c5f834f25fa1e144519c85d7d26c2b3d7a
Alena Chan <86618362+AlenaResiko@users.noreply.github.com>
2024-08-01 16:11:10 -0400

    Add comments

--- Lambda.m
+++ Lambda.m
@@ -1,6 +1,13 @@
 function [L] = Lambda(q)
-% Returns matrix used in q-dot
-L = [-q(2) -q(3) -q(4);
+% Lambda is Jacobian of quaternion derivative (q-dot) w.r.t. angular velocity (w).
+%
+% Input:
+% q - quaternion [4x1].
+%
+% Return:
+% [4x3] matrix.
+%
+L = 0.5*[-q(2) -q(3) -q(4);
     q(1) -q(4) q(3);
     q(4) q(1) -q(2);
     -q(3) q(2) q(1)];
