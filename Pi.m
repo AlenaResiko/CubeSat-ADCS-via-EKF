af1e793461ad2371d118e05fb1ddd95f14bd2672
Alena Chan <86618362+AlenaResiko@users.noreply.github.com>
2024-08-01 16:10:53 -0400

    Add comments

--- Pi.m
+++ Pi.m
@@ -1,6 +1,14 @@
-function [P_out] = Pi(w,J1,J2,J3)
-% J1, J2, J3 are constants
-P_out = [0 (J2-J3)/J1*w(3) (J2-J3)/J1*w(2);
+function [Pi_out] = Pi(w,J1,J2,J3)
+% Pi is Jacobian of angular acceleration (omega-dot) w.r.t. angular velocity (omega).
+%
+% Input:
+% w - angular velocity vector [3x1].
+% J1, J2, J3 - moments of inertia along principal axes.
+%
+% Return:
+% [3x3] matrix.
+%
+Pi_out = [0 (J2-J3)/J1*w(3) (J2-J3)/J1*w(2);
     (J3-J1)/J2*w(3) 0 (J3-J1)/J2*w(1);
     (J1-J2)/J3*w(2) (J1-J2)/J3*w(1) 0];
 end
